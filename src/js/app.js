76199a05a86c168f0b94819d9fa03d1754f37be2
oleh <pshyk.oleg@gmail.com>
2021-11-02 23:24:11 +0200

    hero-section

---
+++ src/js/app.js
@@ -0,0 +1,44 @@
+import 'slick-carousel';
+
+document.addEventListener('DOMContentLoaded', () => { 
+  // console.log("Start APP");
+
+  
+  // $('.js-notable-slider').slick({
+  //   arrows: false,
+  //   autoplay: true,
+  //   dots: true,
+  //   autoplaySpeed: 10000,
+  //   respondTo: 'window',    
+  //   initialSlide: 0,
+  //   infinite: true,
+  //   nextArrow: '.next-notable-slick-btn',
+  //   prevArrow: '.previous-notable-slick-btn',
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,   
+  //   mobileFirst: true,    
+  //   responsive: [
+  //     {
+  //       breakpoint: 815,
+  //       settings: {
+  //         arrows: true, 
+  //         slidesToShow: 2,  
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 1199,
+  //       settings: {      
+  //         arrows: true,    
+  //         slidesToShow: 3,  
+  //       }
+  //     }
+  //   ] 
+  // })
+  // $('.js-notable-slider').css({"opacity": "1","transition": "opacity 0.5s ease"});
+
+  
+
+});
+if (module.hot) {
+  module.hot.accept();
+}
