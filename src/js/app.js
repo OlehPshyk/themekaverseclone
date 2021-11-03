204b12d91a5db3e6b2e6761120d3f33c475c0822
oleh <pshyk.oleg@gmail.com>
2021-11-04 00:02:26 +0200

    run

--- src/js/app.js
+++ src/js/app.js
@@ -3,40 +3,29 @@ import 'slick-carousel';
 document.addEventListener('DOMContentLoaded', () => { 
   // console.log("Start APP");
 
-  
-  // $('.js-notable-slider').slick({
-  //   arrows: false,
-  //   autoplay: true,
-  //   dots: true,
-  //   autoplaySpeed: 10000,
-  //   respondTo: 'window',    
-  //   initialSlide: 0,
-  //   infinite: true,
-  //   nextArrow: '.next-notable-slick-btn',
-  //   prevArrow: '.previous-notable-slick-btn',
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,   
-  //   mobileFirst: true,    
-  //   responsive: [
-  //     {
-  //       breakpoint: 815,
-  //       settings: {
-  //         arrows: true, 
-  //         slidesToShow: 2,  
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 1199,
-  //       settings: {      
-  //         arrows: true,    
-  //         slidesToShow: 3,  
-  //       }
-  //     }
-  //   ] 
-  // })
-  // $('.js-notable-slider').css({"opacity": "1","transition": "opacity 0.5s ease"});
 
+
+  $('.js-gallery-slider').slick({
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 6000,
+    // respondTo: 'window',    
+    initialSlide: 0,
+    infinite: true,    
+    // slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    touchMove: true,
+    variableWidth: true,
+    pauseOnHover: false,
+  })
   
+  $('.js-gallery-slider').css({"opacity": "1","transition": "opacity 0.5s ease"});
+
+  $('.gallery-item').on('click',function() {    
+    $('.js-gallery-slider').slick("slickNext");
+  });
 
 });
 if (module.hot) {
