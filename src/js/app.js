c271dd4b491218adfd0ae954526d0ebd7bc17e0c
oleh <pshyk.oleg@gmail.com>
2021-11-05 03:07:50 +0200

    f

--- src/js/app.js
+++ src/js/app.js
@@ -27,6 +27,20 @@ document.addEventListener('DOMContentLoaded', () => {
     $('.js-gallery-slider').slick("slickNext");
   });
 
+
+  $('.js-work-slider').slick({
+    arrows: false,
+    autoplay: false,
+    dots: true,
+    initialSlide: 0,
+    infinite: false,    
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    touchMove: false,
+  })
+  
+  $('.js-work-slider').css({"opacity": "1","transition": "opacity 0.5s ease"});
+
 });
 if (module.hot) {
   module.hot.accept();
